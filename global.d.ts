7d909e9c653de895ff0b9aae4bfa8a12762a494f
Steve Calvert <scalvert@linkedin.com>
2022-04-12 15:03:42 -0700

    Adding tsup config

--- global.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'bin-links' {
-    const m: ((opts: any) => Promise<void> | Promise<[any, any]>) & {
-        checkBins: any;
-        resetSeen: () => void;
-        getPaths: any;
-    };
-    export = m;
-}
